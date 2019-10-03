10ef708e160e49d422ff2ba1d1f3294462428f29
BruceZoom <wangzhongye1110@qq.com>
2019-10-03 08:45:47 +0800

    include template for the module

---
+++ template/template.c
@@ -0,0 +1,53 @@
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/proc_fs.h>
+#include <linux/uaccess.h>
+
+#define BUFFER_SIZE 128
+#define PROC_NAME "TEMPLATE_PROC"
+
+ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos);
+
+static struct file_operations proc_ops = {
+	.owner = THIS_MODULE,
+	.read = proc_read,
+};
+
+int proc_init(void)
+{
+	proc_create(PROC_NAME, 0666, NULL, &proc_ops);
+	return 0;
+}
+
+void proc_exit(void)
+{
+	remove_proc_entry(PROC_NAME, NULL);
+}
+
+ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos)
+{
+	int rv = 0;
+	char buffer[BUFFER_SIZE];
+	static int completed = 0;
+	
+	if(completed){
+		completed = 0;
+		return 0;
+	}
+	completed = 1;
+	
+	rv = sprintf(buffer, "Hello World!\n");
+	
+	copy_to_user(usr_buf, buffer, rv);
+	
+	return rv;
+}
+
+module_init(proc_init);
+module_exit(proc_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Hello Module");
+MODULE_AUTHOR("BZ");
+
