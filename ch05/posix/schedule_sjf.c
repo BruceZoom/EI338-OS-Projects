9a28af7303fb103d75623f564c4023c0eef3445c
BruceZoom <32293407+BruceZoom@users.noreply.github.com>
2019-11-01 20:42:28 +0800

    implement sjf, switch to linux for debug

---
+++ ch05/posix/schedule_sjf.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "schedulers.h"
+#include "task.h"
+#include "list.h"
+#include "cpu.h"
+
+static struct node *list = NULL;
+static struct task *min_task;
+
+void _add(struct task *t, struct node **target){
+    insert(target, t);
+}
+
+// add a task to the list 
+void add(char *name, int priority, int burst){
+    struct task *t = malloc(sizeof(struct task));
+	t->name = name;
+    t->priority = priority;
+    t->burst = burst;
+    _add(t, &list);
+}
+
+void reverse(struct node *temp){
+    _add(temp->task, &list);
+    free(temp);
+}
+
+void find_next_min(struct node *temp){
+    if(min_task == NULL || min_task->burst > temp->task->burst){
+        min_task = temp->task;
+    }
+}
+
+// invoke the scheduler
+void schedule(){
+    struct node *remain = list->next;
+    list->next = NULL;
+    traverse(remain, reverse);
+    while(list != NULL){
+        min_task = NULL;
+        traverse(remain, find_next_min);
+        run(min_task, min_task->burst);
+        delete(&list, min_task);
+        free(min_task);
+    }
+}
