8da5e5bdfd83820272ea8950f62391023a23a97a
BruceZoom <32293407+BruceZoom@users.noreply.github.com>
2019-11-01 22:23:41 +0800

    implement priority, switch to linux for debug

---
+++ ch05/posix/schedule_priority.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "schedulers.h"
+#include "task.h"
+#include "list.h"
+#include "cpu.h"
+
+static struct node *list = NULL;
+static struct task *prior_task;
+
+void _add(struct task *t, struct node **target){
+    insert(target, t);
+}
+
+// add a task to the list 
+void add(char *name, int priority, int burst){
+    struct task *t = malloc(sizeof(struct task));
+	t->name = name;
+    t->priority = priority;
+    t->burst = burst;
+    _add(t, &list);
+}
+
+void reverse(struct node *temp){
+    _add(temp->task, &list);
+    free(temp);
+}
+
+void find_next_prior(struct node *temp){
+    if(prior_task == NULL || prior_task->priority < temp->task->priority){
+        prior_task = temp->task;
+    }
+}
+
+// invoke the scheduler
+void schedule(){
+    struct node *remain = list->next;
+    list->next = NULL;
+    traverse(remain, reverse);
+    while(list != NULL){
+        prior_task = NULL;
+        traverse(list, find_next_prior);
+        run(prior_task, prior_task->burst);
+        delete(&list, prior_task);
+        free(prior_task);
+    }
+}
